02f6db4cca8efaaccdb2493736ad863d55a7acf0
Noor Muhammad <connectwithnoor1@gmail.com>
2023-07-12 17:59:28 +0500

    added: global error boundary

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { GistProvider } from "./store/gistContext";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GistProvider>
-      <App />
-    </GistProvider>
+    <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <GistProvider>
+        <App />
+      </GistProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("root")
 );
