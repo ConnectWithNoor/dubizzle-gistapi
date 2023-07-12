c717ea14715f6ce1689f4b8ab45fcc28dc835527
Noor Muhammad <connectwithnoor1@gmail.com>
2023-07-12 17:54:53 +0500

    added: gistList to context

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,30 +1,49 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  memo,
+  useContext,
+} from "react";
 import styled from "styled-components";
 
-import { getPublicGists } from "../services/gistService";
 import Gist from "./Gist";
+
+import { getPublicGists } from "../services/gistService";
+import { gistContext } from "../store/gistContext";
+import { ADD_PUBLIC_GISTS } from "../store/actions";
 
 const GistList = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [gistList, setGistList] = useState([]);
+
+  const { gistList, dispatch, error, setError } = useContext(gistContext);
 
   const fetchPublicGists = useCallback(async () => {
     try {
       setIsLoading(true);
+      setError(null);
 
       const publicGists = await getPublicGists();
 
       if (publicGists?.data) {
-        setGistList([...publicGists.data]);
+        const payload = {
+          user: "public",
+          data: publicGists?.data,
+        };
+
+        dispatch({
+          type: ADD_PUBLIC_GISTS,
+          payload,
+        });
       } else {
         throw new Error("Something went wrong.");
       }
     } catch (err) {
-      console.log("something went wrong", err);
+      setError(err);
     } finally {
       setIsLoading(false);
     }
-  }, []);
+  }, [dispatch, setError]);
 
   useEffect(() => {
     fetchPublicGists();
@@ -34,6 +53,8 @@ const GistList = () => {
     <Wrapper>
       {isLoading ? (
         <div> Loading... </div>
+      ) : error ? (
+        <div>{error?.message}</div>
       ) : gistList?.length > 0 ? (
         gistList.map((gistItem) => <Gist key={gistItem.id} gist={gistItem} />)
       ) : (
@@ -52,4 +73,4 @@ const Wrapper = styled.div`
   gap: 1rem;
 `;
 
-export default GistList;
+export default memo(GistList);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { GistProvider } from "./store/gistContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GistProvider>
+      <App />
+    </GistProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
